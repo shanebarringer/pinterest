19d1b4dba302815c3f58f11ed66f5b0620a1dab8
Shane Barringer <shane.barringer@outlook.com>
2014-12-10 09:18:37 -0500

    Pagination on boards

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -4,7 +4,7 @@ class BoardsController < ApplicationController
   # GET /boards
   # GET /boards.json
   def index
-    @boards = Board.all
+    @boards = Board.paginate(page: params[:page], per_page: 5)
   end
 
   # GET /boards/1
